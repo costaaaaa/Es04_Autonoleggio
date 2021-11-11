aa726e80f2e2b9f7c8c0421ce55b4f9f12f0f32c
Stud. Andrea Costamagna <a.costamagna.1294@vallauri.net>
2021-11-11 08:25:36 +0100

    Aggiungere i file di progetto.

---
+++ Es04_Autonoleggio/App_Code/clsDB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//
+using System.Security.Cryptography;
+using System.Text;
+using ADOSQLServer2017_ns;
+
+namespace Es04_Autonoleggio
+{
+    public class clsDB
+    {
+        ADOSQLServer2017 ado;
+        public clsDB(string nomeDB)
+        {
+            this.ado = new ADOSQLServer2017(nomeDB);
+        }
+
+        public string sha256(string pwd)
+        {
+            string p = "";
+            SHA256 mySHA256 = SHA256.Create();
+            //calcolo codice hash 
+            byte[] hashValue = mySHA256.ComputeHash(Encoding.UTF8.GetBytes(pwd));
+            // Convert byte array to a string   
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < hashValue.Length; i++)
+            {
+                builder.Append(hashValue[i].ToString("x2")); //converto in esadecimale
+            }
+            p = builder.ToString();
+            return p;
+        }
+
+    }
+}
---
+++ Es04_Autonoleggio/MasterPage.Master.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Es04_Autonoleggio
+{
+    public partial class MasterPage : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void lnkHome_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("home.aspx", false);
+        }
+
+        protected void lnkLogin_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("login.aspx", false);
+        }
+
+        protected void lnkLogout_Click(object sender, EventArgs e)
+        {
+            Session.Abandon();
+            Response.Redirect("home.aspx", false);
+        }
+    }
+}
